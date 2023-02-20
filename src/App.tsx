f34877a9b87a9f1e5bd6465160604f5ea17c769c
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-02-20 20:21:48 +0000

    edit background

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import "reactflow/dist/style.css";
 
 function App() {
   return (
-    <ReactFlow>
-      <Background />
-      <Controls />
-    </ReactFlow>
+    <div style={{width: '100vw', height: '100vh'}}>
+      <ReactFlow>
+        <Background
+          gap={12}
+          size={2}
+          color="#ddd"
+        />
+      </ReactFlow>
+    </div>
   );
 }
 
